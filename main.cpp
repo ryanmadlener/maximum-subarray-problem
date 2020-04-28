06dcd491fb2407c291dbca27882e24d0b8cb781e
Ryan Madlener <ryanmadlener@hotmail.com>
2020-04-27 18:33:50 -0700

    Improving Readability

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 #include "Processing.h"
 
-// Function that calculates the elapsed time for functions
+
 void calculateTime(std::chrono::time_point<std::chrono::system_clock> startTime,
                         std::chrono::time_point<std::chrono::system_clock> endTime);
 
@@ -12,7 +12,7 @@ int main() {
     Processing calculate;
 
     // Initial size of test array
-    int size = 1000;
+    int arrSize = 1000;
 
     // Variables for timing
     std::chrono::time_point<std::chrono::system_clock> startTime;
@@ -21,21 +21,21 @@ int main() {
     // Setting the random seed
     std::srand(std::time(nullptr));
 
-    // Loop that executes five times changing the
+    // Loop that executes five times increasing the
     // size of the test arrays
     for (int x = 0; x < 5; x++) {
         // Array that will be processed
-        int arr[size];
+        int arr[arrSize];
 
         // Filling the array with random number from -50 to 50
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < arrSize; i++) {
             int randNum = (rand() % 50) - 25;
             arr[i] = randNum;
         }
 
         // Timing for the brute force algorithm
         startTime = std::chrono::high_resolution_clock::now();
-        std::cout << "Brute Force: " << calculate.bruteForce(arr, size) << std::endl;
+        std::cout << "Brute Force: " << calculate.bruteForce(arr, arrSize) << std::endl;
         endTime = std::chrono::high_resolution_clock::now();
 
         // Calling function that calculates time
@@ -43,14 +43,14 @@ int main() {
 
         // Timing for Kadane's Algorithm
         startTime = std::chrono::high_resolution_clock::now();
-        std::cout << "Kadane's: " << calculate.kadane(arr, size) << std::endl;
+        std::cout << "Kadane's: " << calculate.kadane(arr, arrSize) << std::endl;
         endTime = std::chrono::high_resolution_clock::now();
 
         // Calling function that calculates time
         calculateTime(startTime, endTime);
 
         // Incrementing size of array for more tests
-        size += 10000;
+        arrSize += 10000;
     }
 
     return(0);
@@ -66,9 +66,12 @@ int main() {
 void calculateTime(std::chrono::time_point<std::chrono::system_clock> startTime,
                         std::chrono::time_point<std::chrono::system_clock> endTime) {
 
-    std::chrono::duration<double> elapsedSeconds;
+    // Variable for time
+    std::chrono::duration<double> elapsedSeconds{};
 
+    // Calculating time
     elapsedSeconds = endTime - startTime;
+
     std::cout << "Time in Seconds: " << elapsedSeconds.count()
               << std::endl << std::endl;
 }
